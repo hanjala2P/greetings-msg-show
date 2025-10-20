018ceaadcbb6b804b8f296fdf0308f15bb1a618e
hanjala2p <mdhanjala016pc@gmail.com>
2025-10-20 15:40:38 +0600

    checking

---
+++ scrpt.js
@@ -0,0 +1,11 @@
+    setTimeout(function() {
+      const el = document.getElementById('msg');
+      el.textContent = 'welcome';
+      el.classList.add('show'); // fade-in effect (ঐচ্ছিক)
+    }, 4000);
+    
+setTimeout(function(){
+    const txt = document.getElementById('mssg');
+    txt.textContent=' thank you';
+    txt.classList.add('show'); // fade-in effect (ঐচ্ছিক)
+},8000);                                                                            
